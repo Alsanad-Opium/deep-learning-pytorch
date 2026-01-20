efb9271d91a355c0bc7e97c1a3d0978b1de752f3
Alsanad-Opium <alsanad112006@gmail.com>
2026-01-21 00:28:35 +0530

    Added comments to the code for future

--- Week1/Day2/Machine Learning/Simple_Model_Training/app.py
+++ Week1/Day2/Machine Learning/Simple_Model_Training/app.py
@@ -2,36 +2,43 @@ from flask import Flask,request,jsonify, render_template
 import numpy as np 
 import pickle
 
-app = Flask(__name__)
+app = Flask(__name__) # initializing a flask app
 
-model = pickle.load(open("placement_model.pkl","rb"))
+
+## laoding the models 
+model = pickle.load(open("placement_model.pkl","rb")) 
 scalar = pickle.load(open("scalar.pkl", "rb"))
 
 
 
-@app.route('/')
+@app.route('/') ## route to display the home page app.route is a decorator 
 
 def home():
     return render_template('index.html')
 
 
-@app.route('/predict',methods = ['POST'])
+@app.route('/predict',methods = ['POST']) ## this is import for routing  the POSt request that will be sent fro the index.html
+## form will send the data via the action attribute of the form 
 def predict():
     
-    cgpa = float(request.form['cgpa'])
+    cgpa = float(request.form['cgpa']) ## fetching the data from the form
     iq = float(request.form['iq'])
     
-    input_data = np.array([[cgpa,iq]])
-    scaled_data = scalar.transform(input_data)
+    input_data = np.array([[cgpa,iq]]) # converting the data into numpy array as we 
+    # are using the model which was trained on numpy array 
     
-    prediction = model.predict(scaled_data)[0]
+     ## scaling the input data
+    scaled_data = scalar.transform(input_data) ## as the model was trained on scaled data therefore 
+    # we have to scale the input aswell 
+    
+    prediction = model.predict(scaled_data)[0]  ## making the predictio aand as the it is by default predicted as the numpy array therefore we are fetching the first value
     
     if prediction == 1:
         result = "can get job"
     else:
         result = "Wont get a job"
         
-    return render_template('index.html',prediction_value = result)
+    return render_template('index.html',prediction_value = result) ## returning the result to the index.html page ## this will refresh the page 
 
 
 if __name__ == "__main__":
