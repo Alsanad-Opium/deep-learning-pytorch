c97ebda1a712feeecafadb71e24d4cb8ccd44710
Alsanad-Opium <alsanad112006@gmail.com>
2026-06-26 01:11:12 +0530

    predic trotue made bug fixed

--- MLandDL/ML/Machine Learning/Algorithms/SVM/smspredict/app/routes/predict.py
+++ MLandDL/ML/Machine Learning/Algorithms/SVM/smspredict/app/routes/predict.py
@@ -1,8 +1,19 @@
 from flask import request,jsonify,Blueprint
-
+from app import model
 predict_bp = Blueprint('predict',__name__,url_prefix= '/predict')
 
 
-@predict_bp.route('/', method = ['POST'], strict_slashes = False)
+
+@predict_bp.route('/', methods = ['POST'], strict_slashes = False)
 def predict():
+    data  = request.get_json()
+    
+    text = data.get('text')
+    if not text:
+        return jsonify({'message':"Invalid Input"}),400
+    
+    
+    result = model.predict([text])[0]
+    
+    return jsonify({"message":f"The input {data} is {result}"})
     
